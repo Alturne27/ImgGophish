a3d583bd9b960ee85a8814fd1d8d2d53377b2ac1
Alturne27 <119225147+Alturne27@users.noreply.github.com>
2024-11-12 16:56:14 +0100

    Update CapsLock.ps1

--- Script Powershell/CapsLock.ps1
+++ Script Powershell/CapsLock.ps1
@@ -1,7 +1,7 @@
 $wsh = New-Object -ComObject WScript.Shell
 
 while ($true) {
-    Start-Sleep -Seconds 45
+    Start-Sleep -Seconds 5
     $wsh.SendKeys('{CAPSLOCK}')
     Start-Sleep -Seconds 15
     $wsh.SendKeys('{CAPSLOCK}')
